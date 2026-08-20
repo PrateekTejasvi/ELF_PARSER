01f1d629f0df0ae47cfaee1b74f6b60df9bf27c1
PrateekTejasvi <prateek.tejasvi@gmail.com>
2026-08-20 09:37:45 +0530

    added 64bit elf file header and reading into it

--- include/elf.h
+++ include/elf.h
@@ -4,46 +4,39 @@
 
 #ifndef ELF_PARSER_ELF_H
 #define ELF_PARSER_ELF_H
+#include <stdbool.h>
 #include <stdint.h>
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdio.h>
 
- //ELF_PARSER_ELF_H
+// ELF_PARSER_ELF_H
 #define EI_NIDENT 16
-typedef uint32_t Address; //depending on elf class can either be 32 bit or 64 bit
-typedef uint16_t HalfWord;
-typedef uint32_t Offset;
-typedef uint32_t Word;
 /*
  * for e_type
  * ET_NONE 0 = No file type
  * ET_REL 1 = Relocatable file
- * ET_Exec 2 = exectuable file will not support ASLR (address space layout randomization)
- * ET_DYN 3 = shared file
- * ET_CORE 4 = core file
+ * ET_Exec 2 = exectuable file will not support ASLR (address space layout
+ * randomization) ET_DYN 3 = shared file ET_CORE 4 = core file
  */
-static unsigned char MAGIC[4] = {0x7f,'E','L','F'};
+const static unsigned char MAGIC[4] = {0x7f, 'E', 'L', 'F'};
 bool elf_isvalid(const uint8_t *data);
 
-uint8_t getelf_class(FILE* file);
-uint8_t getelf_data(FILE *file);
 
 typedef struct ELFHeader {
-    unsigned char e_ident[EI_NIDENT];
-    HalfWord e_type;//type of the elf file
-    HalfWord e_machine;//machine type
-    Word e_version;//version always 1
-    Address e_entry;//entry point for the exectuables
-    Offset e_phoff;//program headers offset
-    Offset e_shoff;//section headers offset
-    Word e_flags;
-    HalfWord e_ehsize;//size
-    HalfWord e_phentsize;//size per program header
-    HalfWord e_phnum;//number of program headers
-    HalfWord e_shentsize;
-    HalfWord e_shnum;
-    HalfWord e_shstrndx;
+  unsigned char e_ident[EI_NIDENT];
+  uint16_t e_type;    // type of the elf file
+  uint16_t e_machine; // machine type
+  uint32_t e_version;     // version always 1
+  uint64_t e_entry;    // entry point for the exectuables
+  uint64_t e_phoff;     // program headers offset
+  uint64_t e_shoff;     // section headers offset
+  uint32_t e_flags;
+  uint16_t e_ehsize;    // size
+  uint16_t e_phentsize; // size per program header
+  uint16_t e_phnum;     // number of program headers
+  uint16_t e_shentsize;
+  uint16_t e_shnum;
+  uint16_t e_shstrndx;
 
-}ELFHeader;
+} ELFHeader;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdint.h>
 #include<stdio.h>
 #include<stdbool.h>
@@ -17,39 +18,36 @@ uint8_t getelf_class(FILE *file){
     fread(&data,sizeof(data),1,file);
     return data;
 }
-uint8_t getelf_data(FILE *file){
-    uint8_t elf_data;
-    fseek(file,5L,SEEK_SET);
-    fread(&elf_data,sizeof(elf_data),1,file);
-    return elf_data;
-}
-
+ELFHeader readheader(FILE *file){
+    ELFHeader data;
+    fseek(file,0L,SEEK_SET);
+    fread(&data,sizeof(ELFHeader),1,file);
+    return data;
+    }
 int main(int argc,char **argv) {
     if(argc != 2){
         printf("usage: %s <elf-file>\n",argv[0]);
         return EXIT_FAILURE;
     }
        FILE *file = fopen(argv[1],"rb");
+       if(getelf_class(file)==1){
+           printf("ELF32 not supported!\n");
+           return EXIT_FAILURE;
+       }
        if(file == NULL){
            perror("Error opening file");
            return EXIT_FAILURE;
        }
-       uint8_t buffer[BUFFER_SIZE];
-       size_t bytesRead = fread(buffer,sizeof(uint8_t),BUFFER_SIZE,file);
-       if (bytesRead < 4) {
-           printf("File too small to be an ELF file\n");
-           fclose(file);
-           return EXIT_FAILURE;
-       }
-       if(elf_isvalid(buffer)){
-           printf("file is a valid elf\n");
-       }
-       else{
-           printf("file is not a valid elf file\n");
-       }
-       printf("elf_class:%d\n",getelf_class(file));
-       printf("elf_data:%d\n",getelf_data(file));//TODO:do not need this technically because we have already loaded the first 16 bytes of the file into the buffer
-       printf("elf_version:%d\n",buffer[6]);
+       ELFHeader header = readheader(file);
+       printf("%zu\n",sizeof(ELFHeader));
+       printf("Type: %u\n", header.e_type);
+       printf("Machine: %u\n", header.e_machine);
+       printf("Version: %u\n", header.e_version);
+       printf("Entry: 0x%llx\n", (unsigned long long)header.e_entry);
+       printf("Program Header Offset: 0x%llx\n",
+              (unsigned long long)header.e_phoff);
+       printf("Section Header Offset: 0x%llx\n",
+              (unsigned long long)header.e_shoff);
        fclose(file);
     return 0;
 }
